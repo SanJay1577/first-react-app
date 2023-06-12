514f9233edb93b60a525dd8ad3ca9d467c5826a5
SanJay1577 <sanjayprashanth1997@gmail.com>
2023-06-12 10:35:35 +0530

    last-commit-for-12

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import TopBar from './Components/TopBar';
 import Base from './BasePage/Base';
+import Dashboard from './Pages/Dashboard';
+import Profile from './Pages/Profile';
 
 
 function App() {
 
 
+const studentData = [
+  {
+  name : "sanjay",
+  age : 25
+  },
+  {
+    name : "ajay",
+    age : 23
+    }
+]
+const [data, setData] = useState(studentData)
   return (
 <div className="App">
-  <Base/>
+  <Dashboard
+   data ={data}
+   setData ={setData}
+  />
 </div>
   ); 
 }
@@ -20,5 +36,7 @@ export default App;
 
 
 
+// useState() - day1
+// useEffect() -
 
 
--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -2,23 +2,23 @@ export default function SideBar(){
     return(
     
            <div className="side-bar-styles p-7">
-            <div className="actions">
-              <ul>
-                <li>
-                    <button>Admin</button>
+            <div>
+              <ul className="actions">
+                <li className="rounded-full p-2 hover:bg-sky-700">
+                    <button>Dashboard</button>
                 </li>
-                <li>
-                    <button>user</button>
+                <li className="rounded-full p-2 hover:bg-sky-700">
+                    <button >Profile</button>
                 </li>
-                <li>
+                <li className="rounded-full p-2 hover:bg-sky-700">
                     <button>Student list</button>
                 </li>
-                <li>
+                <li className="rounded-full p-2 hover:bg-sky-700">
                     <button>Interview notes</button>
                 </li>
                 </ul>
             </div>        
-            <div className="logout-btn">
+            <div className="logout-btn rounded-full hover:bg-sky-700 p-2" >
                     <button>logout</button>
             </div>
         </div>
