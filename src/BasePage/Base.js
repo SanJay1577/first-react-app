a2c0c56e43e78b186c3df56dc661fe3b3f6df220
SanJay1577 <sanjayprashanth1997@gmail.com>
2023-06-12 09:34:49 +0530

    chnages in sidebar

--- src/BasePage/Base.js
+++ src/BasePage/Base.js
@@ -11,9 +11,8 @@ export default function Base(){
         {/* <div className="grid h-20 card bg-base-300 rounded-box place-items-center"> */}
 
         <div className="flex flex-nowrap rounded-box place-items-center h-screen">
-  <div className="grid flex-grow h-full w-1/5 card rounded-box place-items-center">
+  <div className="grid flex-grow h-full w-1/5 card rounded-box place-items-center bg-base-300 m-5">
       <SideBar/> 
-      <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">"open"</label>
     </div> 
   <div className="divider lg:divider-horizontal"></div> 
   <div className="grid flex-grow h-32 w-4/5 card bg-base-300 rounded-box place-items-center">content</div>
--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -1,25 +1,26 @@
 export default function SideBar(){
     return(
-        <div>
-                
-            <div className="drawer lg:drawer-open">
-  <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-  <div className="drawer-content flex flex-col items-center justify-center">
-    {/* Page content here */}
-  
-  </div> 
-  <div className="drawer-side">
-    <label htmlFor="my-drawer-2" className="drawer-overlay"></label> 
-    <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content">
-      {/* Sidebar content here */}
-      <li><button>Dashboard</button></li>
-      <li><button>Studnent List</button></li>
-      <li><button>Interview Notes</button></li>
-      <li><button>Logout</button></li>
-    </ul>
-  
-  </div>
-</div>
+    
+           <div className="side-bar-styles p-7">
+            <div className="actions">
+              <ul>
+                <li>
+                    <button>Admin</button>
+                </li>
+                <li>
+                    <button>user</button>
+                </li>
+                <li>
+                    <button>Student list</button>
+                </li>
+                <li>
+                    <button>Interview notes</button>
+                </li>
+                </ul>
+            </div>        
+            <div className="logout-btn">
+                    <button>logout</button>
+            </div>
         </div>
     )
 }
