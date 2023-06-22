2e107cdca79573247ad31696f205c3a20dfeccd0
SanJay1577 <sanjayprashanth1997@gmail.com>
2023-06-22 10:18:17 +0530

    pagination intial steps

--- src/Components/StudentList.js
+++ src/Components/StudentList.js
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import Base from "../BasePage/Base"
 import { AppState } from "../Context/AppProvider"
 import CrumBar from "./CrumBar"
@@ -5,15 +6,28 @@ import StudentCard from "./StudentCard"
 
 function StudentList(){
     const {studentData} = AppState()
+
+
+
     return (
         <Base>
         <CrumBar/>
-            {studentData.map((stud, idx)=>(
+            {studentData.slice(0, 2).map((stud, idx)=>(
                 <StudentCard
                 student={stud}
                 key={idx}
                 />
             ))}
+            
+<div className="join">
+  {
+    [...Array(Math.ceil((studentData.length/2)))].map((_, index)=>(
+        <button
+         className={`join-item btn btn-md `}>{index+1}</button>     
+    ))
+  }
+
+</div>
         </Base>
     )
 }
